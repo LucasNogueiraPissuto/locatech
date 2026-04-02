1bc5c54b7ba77d0e4ea3a13f30c6577d11b4ecc6
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2026-04-02 17:04:15 -0300

    Adicionando camada de repository para o veiculo

---
+++ src/main/java/br/com/lucasP/locatech/locatech/repositories/VeiculoRepositoryImp.java
@@ -0,0 +1,74 @@
+package br.com.lucasP.locatech.locatech.repositories;
+
+import br.com.lucasP.locatech.locatech.entities.Veiculo;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.web.servlet.function.RequestPredicates.param;
+
+@Repository
+public class VeiculoRepositoryImp implements VeiculoRepository {
+
+    private final JdbcClient jdbcClient;
+
+    public VeiculoRepositoryImp(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Override
+    public Optional<Veiculo> findById(Long id) {
+        return this.jdbcClient
+                .sql("SELECT * FROM veiculos WHERE id = :id")
+                .param("id", id)
+                .query(Veiculo.class)
+                .optional();
+    }
+
+    @Override
+    public List<Veiculo> findAll(int size, int Offset) {
+        return this.jdbcClient
+                .sql("SELECT * FROM veiculos LIMIT :size OFFSET :offset")
+                .param("size", size)
+                .param("offset", Offset)
+                .query(Veiculo.class)
+                .list();
+    }
+
+    @Override
+    public Integer save(Veiculo veiculo) {
+        return this.jdbcClient
+                .sql("INSERT INTO veiculos (marca, modelo, placa, ano, cor, valor_diaria) VALUES (:marca, :modelo, :placa, :ano, :cor, :valorDiaria)")
+                .param("marca", veiculo.getMarca())
+                .param("modelo", veiculo.getModelo())
+                .param("placa", veiculo.getPlaca())
+                .param("ano", veiculo.getAnoFabricacao())
+                .param("cor", veiculo.getCor())
+                .param("valorDiaria", veiculo.getValorDiaria())
+                .update();
+    }
+
+    @Override
+    public Integer update(Veiculo veiculo, Long id) {
+        return this.jdbcClient
+                .sql("UPDATE veiculos SET marca = :marca, modelo = :modelo, placa = :placa, ano = :ano, cor = :cor, valor_diaria = :valorDiaria WHERE id = :id")
+                .param("id", id)
+                .param("marca", veiculo.getMarca())
+                .param("modelo", veiculo.getModelo())
+                .param("placa", veiculo.getPlaca())
+                .param("ano", veiculo.getAnoFabricacao())
+                .param("cor", veiculo.getCor())
+                .param("valorDiaria", veiculo.getValorDiaria())
+                .update();
+    }
+
+    @Override
+    public Integer delete(Long id) {
+        return this.jdbcClient
+                .sql("DELETE FROM veiculos WHERE id = :id")
+                .param("id", id)
+                .update();
+    }
+}
