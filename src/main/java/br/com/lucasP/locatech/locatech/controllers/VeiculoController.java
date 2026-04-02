8439700847fbbe00b4f7fda19be3b7218a77d71f
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2026-04-02 18:12:51 -0300

    Alterando controller

--- src/main/java/br/com/lucasP/locatech/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/lucasP/locatech/locatech/controllers/VeiculoController.java
@@ -1,16 +1,39 @@
 package br.com.lucasP.locatech.locatech.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.lucasP.locatech.locatech.entities.Veiculo;
+import br.com.lucasP.locatech.locatech.services.VeiculoService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VeiculoController.class);
+
+    private final VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService) {
+        this.veiculoService = veiculoService;
+    }
+
     @GetMapping
-    public String getVeiculo() {
-        return "Hello veiculo!";
+    public ResponseEntity<List<Veiculo>> findAllVeiculos(@RequestParam("page") int page, @RequestParam("size") int size) {
+        logger.info("/veiculos");
+        var veiculos = this.veiculoService.findAll(page, size);
+        return ResponseEntity.ok(veiculos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Veiculo>> findVeiculoById(@PathVariable("id") Long id) {
+        logger.info("/veiculos/" + id);
+        var veiculo = this.veiculoService.findVeiculoById(id);
+        return ResponseEntity.ok(veiculo);
     }
 
 }
